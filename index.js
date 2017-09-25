227ad6200c380ca014912da3677a70757060eac1
Christina Zakarian <christinazakarian@gmail.com>
2017-09-25 02:35:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const app = "I don't do much."
+
+function kittens() {
+  var kittens = ["Milo", "Otis", "Garfield"]
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
